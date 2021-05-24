ab9f5a446fc3c4860fde321ea76c470e99ffb10d
Chirag Pipalia <CHIRAGPIPALIA24@GMAIL.COM>
2021-05-24 15:19:26 -0400

    using npz to save cosine sim

--- app.py
+++ app.py
@@ -6,8 +6,10 @@ import difflib
 from sklearn.metrics.pairwise import cosine_similarity
 
 
+
 app = Flask(__name__)
-movie_features = pickle.load(open('movie_features.pickle', 'rb'))
+##movie_features = pickle.load(open('movie_features.pickle', 'rb'))
+cosine_sim = np.load('cosine_sim.npz')['a']
 movies = pickle.load(open('movies.pickle', 'rb'))
 
 @app.route('/')
@@ -27,7 +29,7 @@ def predict():
     movie_idx = dict(zip(movies['newname'], list(movies.index)))
     #title = movie_finder(title_string)
     idx = movie_idx[in_movie]
-    cosine_sim = cosine_similarity(movie_features, movie_features)
+    #cosine_sim = cosine_similarity(movie_features, movie_features)
     sim_scores = list(enumerate(cosine_sim[idx]))
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
     n_recommendations = 5
