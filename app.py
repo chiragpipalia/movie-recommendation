15315d859c5fb020c518494ec763f8444f78ff33
Chirag Pipalia <CHIRAGPIPALIA24@GMAIL.COM>
2021-05-24 14:21:49 -0400

    calculate cosine, not reading pickle

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ from flask import Flask, request, jsonify, render_template
 import pickle
 import pandas as pd
 import difflib
+from sklearn.metrics.pairwise import cosine_similarity
+
 
 app = Flask(__name__)
-cosine_sim = pickle.load(open('cosine_sim.pickle', 'rb'))
+movie_features = pickle.load(open('movie_features.pickle', 'rb'))
 movies = pickle.load(open('movies.pickle', 'rb'))
 
 @app.route('/')
@@ -25,6 +27,7 @@ def predict():
     movie_idx = dict(zip(movies['newname'], list(movies.index)))
     #title = movie_finder(title_string)
     idx = movie_idx[in_movie]
+    cosine_sim = cosine_similarity(movie_features, movie_features)
     sim_scores = list(enumerate(cosine_sim[idx]))
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
     n_recommendations = 5
